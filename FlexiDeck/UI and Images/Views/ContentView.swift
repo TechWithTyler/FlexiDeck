7b10d8c0e75432dc7e3bd4f775e26bee397cb00e
TechWithTyler <twsandmds@gmail.com>
2024-08-09 13:54:19 -0700

    Refactored ContentView

--- FlexiDeck/UI and Images/Views/ContentView.swift
+++ FlexiDeck/UI and Images/Views/ContentView.swift
@@ -36,116 +36,11 @@ struct ContentView: View {
 
     var body: some View {
         NavigationSplitView {
-            ZStack {
-                if decks.count > 0 {
-                    List(selection: $selectedDeck) {
-                        ForEach(decks) { deck in
-                            NavigationLink(deck.name, value: deck)
-                                .contextMenu {
-                                    Button("Settings…", systemImage: "gear") {
-                                        dialogManager.deckToShowSettings = deck
-                                    }
-                                    Button(role: .destructive) {
-                                        dialogManager.deckToDelete = deck
-                                        dialogManager.showingDeleteDeck = true
-                                    } label: {
-                                        Label("Delete…", systemImage: "trash")
-                                            .foregroundStyle(.red)
-                                    }
-                                }
-
-                        }
-                        .onDelete(perform: deleteItems)
-                    }
-                } else {
-                    Text("No decks")
-                        .font(.largeTitle)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.secondary)
-                }
-            }
-            .navigationTitle("FlexiDeck")
-            #if !os(macOS)
-            .navigationBarTitleDisplayMode(.large)
-            #endif
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 300, ideal: 300)
-#endif
-            .alert("Delete this deck?", isPresented: $dialogManager.showingDeleteDeck, presenting: $dialogManager.deckToDelete) { deck in
-                Button("Delete") {
-                    deleteDeck(deck.wrappedValue!)
-                    dialogManager.deckToDelete = nil
-                    dialogManager.showingDeleteDeck = false
-                }
-                Button("Cancel", role: .cancel) {
-                    dialogManager.deckToDelete = nil
-                    dialogManager.showingDeleteDeck = false
-                }
-            }
-            .alert("Delete all decks?", isPresented: $dialogManager.showingDeleteAllDecks) {
-                Button("Delete") {
-                    selectedCard = nil
-                    selectedDeck = nil
-                    for deck in decks {
-                        modelContext.delete(deck)
-                    }
-                    dialogManager.showingDeleteAllDecks = false
-                }
-                Button("Cancel", role: .cancel) {
-                    dialogManager.showingDeleteAllDecks = false
-                }
-            }
-            .toolbar {
-                ToolbarItem {
-                    OptionsMenu(title: .menu) {
-                        Button(action: addItem) {
-                            Label("Add Deck", systemImage: "rectangle.stack.badge.plus")
-                        }
-                        Divider()
-                        Button(role: .destructive) {
-                            dialogManager.showingDeleteAllDecks = true
-                        } label: {
-                            Label("Delete All Decks…", systemImage: "trash.fill")
-                                .foregroundStyle(.red)
-                        }
-                        #if !os(macOS)
-                        Button("Settings…", systemImage: "gear") {
-                            dialogManager.showingSettings = true
-                        }
-                        #endif
-                    }
-                }
-            }
+            sidebar
         } content: {
-            ZStack {
-                if let deck = selectedDeck {
-                    CardListView(deck: deck, selectedCard: $selectedCard)
-                } else {
-                    Text("Select a deck")
-                        .font(.largeTitle)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(.secondary)
-                }
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 300, ideal: 300)
-#endif
+            cardList
         } detail: {
-            ZStack {
-                if selectedDeck != nil {
-                    if let card = selectedCard {
-                        CardView(card: card)
-                    } else {
-                        Text("Select a card")
-                            .font(.largeTitle)
-                            .multilineTextAlignment(.center)
-                            .foregroundStyle(.secondary)
-                    }
-                }
-            }
-#if os(macOS)
-            .navigationSplitViewColumnWidth(min: 500, ideal: 600)
-#endif
+            cardView
         }
         .sheet(item: $dialogManager.deckToShowSettings) { deck in
             DeckSettingsView(deck: deck)
@@ -155,6 +50,126 @@ struct ContentView: View {
             SettingsView()
         }
         #endif
+    }
+
+    @ViewBuilder
+    var sidebar: some View {
+        ZStack {
+            if decks.count > 0 {
+                List(selection: $selectedDeck) {
+                    ForEach(decks) { deck in
+                        NavigationLink(deck.name, value: deck)
+                            .contextMenu {
+                                Button("Settings…", systemImage: "gear") {
+                                    dialogManager.deckToShowSettings = deck
+                                }
+                                Button(role: .destructive) {
+                                    dialogManager.deckToDelete = deck
+                                    dialogManager.showingDeleteDeck = true
+                                } label: {
+                                    Label("Delete…", systemImage: "trash")
+                                        .foregroundStyle(.red)
+                                }
+                            }
+
+                    }
+                    .onDelete(perform: deleteItems)
+                }
+            } else {
+                Text("No decks")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+            }
+        }
+        .navigationTitle("FlexiDeck")
+        #if !os(macOS)
+        .navigationBarTitleDisplayMode(.large)
+        #endif
+#if os(macOS)
+        .navigationSplitViewColumnWidth(min: 300, ideal: 300)
+#endif
+        .alert("Delete this deck?", isPresented: $dialogManager.showingDeleteDeck, presenting: $dialogManager.deckToDelete) { deck in
+            Button("Delete") {
+                deleteDeck(deck.wrappedValue!)
+                dialogManager.deckToDelete = nil
+                dialogManager.showingDeleteDeck = false
+            }
+            Button("Cancel", role: .cancel) {
+                dialogManager.deckToDelete = nil
+                dialogManager.showingDeleteDeck = false
+            }
+        }
+        .alert("Delete all decks?", isPresented: $dialogManager.showingDeleteAllDecks) {
+            Button("Delete") {
+                selectedCard = nil
+                selectedDeck = nil
+                for deck in decks {
+                    modelContext.delete(deck)
+                }
+                dialogManager.showingDeleteAllDecks = false
+            }
+            Button("Cancel", role: .cancel) {
+                dialogManager.showingDeleteAllDecks = false
+            }
+        }
+        .toolbar {
+            ToolbarItem {
+                OptionsMenu(title: .menu) {
+                    Button(action: addItem) {
+                        Label("Add Deck", systemImage: "rectangle.stack.badge.plus")
+                    }
+                    Divider()
+                    Button(role: .destructive) {
+                        dialogManager.showingDeleteAllDecks = true
+                    } label: {
+                        Label("Delete All Decks…", systemImage: "trash.fill")
+                            .foregroundStyle(.red)
+                    }
+                    #if !os(macOS)
+                    Button("Settings…", systemImage: "gear") {
+                        dialogManager.showingSettings = true
+                    }
+                    #endif
+                }
+            }
+        }
+    }
+
+    @ViewBuilder
+    var cardList: some View {
+        ZStack {
+            if let deck = selectedDeck {
+                CardListView(deck: deck, selectedCard: $selectedCard)
+            } else {
+                Text("Select a deck")
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.secondary)
+            }
+        }
+#if os(macOS)
+        .navigationSplitViewColumnWidth(min: 300, ideal: 300)
+#endif
+    }
+
+    @ViewBuilder
+    var cardView: some View {
+        ZStack {
+            if selectedDeck != nil {
+                if let card = selectedCard {
+                    CardView(card: card)
+                } else {
+                    Text("Select a card")
+                        .font(.largeTitle)
+                        .multilineTextAlignment(.center)
+                        .foregroundStyle(.secondary)
+                }
+            }
+        }
+#if os(macOS)
+        .navigationSplitViewColumnWidth(min: 500, ideal: 600)
+#endif
     }
 
     // MARK: - Data Management
