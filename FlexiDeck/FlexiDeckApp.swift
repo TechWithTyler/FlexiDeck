f01c3f116ede1cb0bec4da257a95506322813ef6
TechWithTyler <twsandmds@gmail.com>
2024-08-12 13:44:44 -0700

    Added comments to ModelContainer code

--- FlexiDeck/FlexiDeckApp.swift
+++ FlexiDeck/FlexiDeckApp.swift
@@ -19,16 +19,21 @@ struct FlexiDeckApp: App {
 
     // MARK: - Properties - Model Container
 
+    // Returns the shared model container for the application.
     var sharedModelContainer: ModelContainer = {
+        // 1. Create a schema to define the app's data model.
         let schema = Schema([
             Deck.self,
             Card.self
         ])
+        // 2. Create a ModelConfiguration with the schema.
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
-
         do {
-            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+            // 3. Try to create a model container with the schema and model configuration.
+            let modelContainer = try ModelContainer(for: schema, configurations: [modelConfiguration])
+            return modelContainer
         } catch {
+            // 4. If a model container can't be created, throw a fatal error.
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
