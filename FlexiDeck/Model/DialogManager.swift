9d39271ea08dd9809013d0ca98436f14bae4c2a1
TechWithTyler <twsandmds@gmail.com>
2024-08-12 14:01:04 -0700

    Update DialogManager.swift
    Added comments

--- FlexiDeck/Model/DialogManager.swift
+++ FlexiDeck/Model/DialogManager.swift
@@ -12,26 +12,35 @@ class DialogManager: ObservableObject {
 
     // MARK: - Properties - Booleans
 
+    // Whether the "delete this card" alert should be/is being displayed.
     @Published var showingDeleteCard: Bool = false
 
+    // Whether the "delete this deck" alert should be/is being displayed.
     @Published var showingDeleteDeck: Bool = false
 
+    // Whether the "delete all cards" alert should be/is being displayed.
     @Published var showingDeleteAllCards: Bool = false
 
+    // Whether the "delete all decks" alert should be/is being displayed.
     @Published var showingDeleteAllDecks: Bool = false
 
 #if !os(macOS)
+    // Whether the settings sheet should be/is being displayed.
     @Published var showingSettings: Bool = false
 #endif
 
     // MARK: - Properties - Decks and Cards
 
+    // The card to be deleted.
     @Published var cardToDelete: Card? = nil
 
+    // The deck to be deleted.
     @Published var deckToDelete: Deck? = nil
 
+    // The card whose settings are being changed.
     @Published var cardToShowSettings: Card? = nil
 
+    // The deck whose settings are being changed.
     @Published var deckToShowSettings: Deck? = nil
 
 }
