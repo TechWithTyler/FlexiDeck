fead71f0ac6a74bc8893df9216a7a500eb6eb58e
TechWithTyler <twsandmds@gmail.com>
2025-11-20 13:40:19 -0800

    Update FlexiDeckApp.swift

--- FlexiDeck/Main App Structure/FlexiDeckApp.swift
+++ FlexiDeck/Main App Structure/FlexiDeckApp.swift
@@ -23,21 +23,37 @@ struct FlexiDeckApp: App {
 
     // MARK: - Properties - Model Container
 
-    // Returns the shared model container for the application.
+    // Returns the shared model container for the application. This code uses a file named "default" with extension "store" in the user's Application Support directory.
     var sharedModelContainer: ModelContainer = {
         // 1. Create a schema to define the app's data model.
         let schema = Schema([
             Deck.self,
             Card.self
         ])
-        // 2. Create a ModelConfiguration with the schema.
-        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false, allowsSave: true, cloudKitDatabase: .automatic)
+        // 2. Set the URL for the store.
+        let storeFilename: String = "default"
+        let storeFileExtension: String = "store"
+        let storeManager: FileManager = .default
+        let domain: FileManager.SearchPathDomainMask = .userDomainMask
+        let location: FileManager.SearchPathDirectory = .applicationSupportDirectory
+        // The URLs for the given location in the given domain.
+        let URLs = storeManager.urls(for: location, in: domain)
+        // Gets the last URL from the URLs array and adds a new folder to it.
+        let appSupportURL: URL = URLs.last!
+        // Creates a URL with name "default.store"
+        // Here, a file or directory is inserted into the specified location in the specified domain.
+        // Like with arrays, to append something means "to add."
+        let fileURL: URL = appSupportURL.appending(path: storeFilename, directoryHint: .notDirectory)
+        // Appends ".store" to the URL. This is the full URL that gets returned.
+        let file: URL = fileURL.appendingPathExtension(storeFileExtension)
+        // 3. Create a ModelConfiguration with the schema.
+        let modelConfiguration = ModelConfiguration(schema: schema, url: file, allowsSave: true, cloudKitDatabase: .automatic)
         do {
-            // 3. Try to create a model container with the schema and model configuration.
+            // 4. Try to create a model container with the schema and model configuration.
             let modelContainer = try ModelContainer(for: schema, migrationPlan: FlexiDeckMigrationPlan.self, configurations: [modelConfiguration])
             return modelContainer
         } catch {
-            // 4. If a model container can't be created, throw a fatal error.
+            // 5. If a model container can't be created, throw a fatal error.
             fatalError("Could not create ModelContainer: \(error)")
         }
     }()
